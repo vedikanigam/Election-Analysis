7685ef6d6057b705b3d805b630d302631379cb91
Vedika Nigam <vedika.nigam@gmail.com>
2021-09-29 23:30:03 -0400

    Adding Pypoll.py file

---
+++ Pypoll.py
@@ -0,0 +1,16 @@
+import csv
+import os
+
+file_to_load = os.path.join("Resources","election_results.csv")
+
+file_to_save = os.path.join("analysis","election_analysis2.txt")
+
+with open(file_to_load) as election_data:
+    file_reader = csv.reader(election_data)
+
+    headers = next(file_reader)
+    print(headers)
+    
+    
+   
+
